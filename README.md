d456b29741ffea1ae324db96c7a93e22c0793b94
Braxton Schafer <braxton@cmdcentral.xyz>
2022-11-16 23:04:14 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# zfs-rclone
+
+This script is designed to be dumped into `/etc/cron.hourly`. It uses [rclone](https://rclone.org/) to backup
+ZFS datasets with a specific property on a periodic basis.
+
+## Prerequisites
+
+- Rclone configured with your desired remote for root (or whatever user you care to run this as).
+- Sufficiently modern ZFS that supports user properties
+- Modern `bash`
+
+## Configuration
+
+To configure backups for a ZFS dataset, run
+
+```bash
+zfs set backups:b2:schedule=daily my/dataset/name
+```
+
+All other config is handled via vars set at the top of the script:
+
+| Variable                | Help                                                |
+| --                      | -                                                   |
+| PROCESSED_PROPERTY_NAME | ZFS property with timestmap of last successful sync |
+| SCHEDULE_PROPERTY_NAME  | ZFS property to read for schedule                   |
+| RCLONE_OPTIONS          | These flags are passed to `rclone`                  |
+| RCLONE_REMOTE           | Name of already configured remote in `rclone`       |
+| RCLONE_STRATEGY         | One of `sync`, `copy`, or `move`; see rclone docs   |
+| ZFS_TYPE                | One of `filesystem`, `snap`, `vol`, or `all`        |
+| ZPOOL_NAME              | Name of the base zpool to find datasets in          |
+
+## Multiple remotes or zpools
+
+If you require backing up to multiple rclone remotes, or from multiple zpools, you can run multiple instancse of the script.
+So long as `SCHEDULE_PROPERTY_NAME` and `PROCESSED_PROPERTY_NAME` are distinct for instances running on a zpool, there
+should be no conflict.
+
+Or, I'd happily welcome a PR to improve this :)
+
---
+++ zfs-rclone.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2154
+set -x
+
+PROCESSED_PROPERTY_NAME='backups:b2:last_processed'
+SCHEDULE_PROPERTY_NAME='backups:b2:schedule'
+RCLONE_OPTIONS=('--fast-list' '--progress' '--one-file-system') # '--syslog' 
+RCLONE_REMOTE='b2_crypt'
+RCLONE_STRATEGY='sync' # or copy, or move
+ZFS_TYPE='filesystem' # or snap, or all, or vol
+ZPOOL_NAME='warp'
+
+declare -A VALID_SCHEDULES
+VALID_SCHEDULES[hourly]=$((60*60))
+VALID_SCHEDULES[daily]=$((hourly*24))
+VALID_SCHEDULES[weekly]=$((daily*7))
+VALID_SCHEDULES[monthly]=$((weekly*4))
+
+# helper functions
+should_process() {
+    local schedule="$1"
+    local last_processed="$2"
+
+    local now
+    now=$(date +%s)
+
+    [[ $((now-last_processed)) -ge "${VALID_SCHEDULES[$schedule]}" ]]
+}
+
+exec_rclone() {
+    local fsname="$1"
+
+    echo "Running rclone:" "$RCLONE_STRATEGY" "${RCLONE_OPTIONS[@]}" "$fsname" "${RCLONE_REMOTE}:${fsname}"
+    rclone "$RCLONE_STRATEGY" "${RCLONE_OPTIONS[@]}" "/${fsname}" "${RCLONE_REMOTE}:${fsname}"
+}
+
+# get all candidates for processing
+candidates=$(zfs get -H -o name,value -r "$SCHEDULE_PROPERTY_NAME" "$ZPOOL_NAME" -t "$ZFS_TYPE" | awk '$2 != "-"')
+
+while read -r fsname schedule ; do
+    if [[ ! -v "VALID_SCHEDULES[${schedule}]" ]]; then
+        echo "Invalid schedule $schedule set on $fsname; skipping" >&2
+        continue
+    fi
+
+    last_processed=$(zfs get -H -o value "$PROCESSED_PROPERTY_NAME" "$fsname")
+    last_processed="${last_processed/-/0}"  # if not set, assume it's never been processed
+
+    if should_process "$schedule" "$last_processed"; then
+        exec_rclone "$fsname"
+        zfs set "$PROCESSED_PROPERTY_NAME"="$(date +%s)" "$fsname"
+    else
+        echo "Skipping $fsname as it was last processed on $(date -d="$last_processed" +%c) and schedule is $schedule"
+    fi
+
+done <<< "$candidates"
