02c4c654628a2874fbe350e618efb01da78ad35b
Braxton Schafer <braxton@cmdcentral.xyz>
2022-12-17 12:09:04 -0600

    feat: add locking per-dataset + remote
    Ensure that only one backup job per dataset + remote pair can run at
    a time.
    
    Fixes #1

--- zfs-rclone.sh
+++ zfs-rclone.sh
@@ -8,21 +8,25 @@ set -x
 
 PROCESSED_PROPERTY_NAME='backups:b2:last_processed'
 SCHEDULE_PROPERTY_NAME='backups:b2:schedule'
-RCLONE_OPTIONS=('--fast-list' '--progress' '--one-file-system') # '--syslog' 
+RCLONE_OPTIONS=('--fast-list' '--progress' '--one-file-system') # '--syslog'
 RCLONE_REMOTE='b2_crypt'
 RCLONE_STRATEGY='sync' # or copy, or move
-ZFS_TYPE='filesystem' # or snap, or all, or vol
+ZFS_TYPE='filesystem'  # or snap, or all, or vol
 ZPOOL_NAME='warp'
 
 ####################
 # INTERNAL OPTIONS #
 ####################
 
+LOCK_TIMEOUT=90
+LOCKFILE_DIRECTORY='/var/lock/zfs-rclone'
+mkdir -p "$LOCKFILE_DIRECTORY"
+
 declare -A VALID_SCHEDULES
-VALID_SCHEDULES[hourly]=$((60*60))
-VALID_SCHEDULES[daily]=$((hourly*24))
-VALID_SCHEDULES[weekly]=$((daily*7))
-VALID_SCHEDULES[monthly]=$((weekly*4))
+VALID_SCHEDULES[hourly]=$((60 * 60))
+VALID_SCHEDULES[daily]=$((hourly * 24))
+VALID_SCHEDULES[weekly]=$((daily * 7))
+VALID_SCHEDULES[monthly]=$((weekly * 4))
 
 ####################
 # HELPER FUNCTIONS #
@@ -35,14 +39,22 @@ should_process() {
     local now
     now=$(date +%s)
 
-    [[ $((now-last_processed)) -ge "${VALID_SCHEDULES[$schedule]}" ]]
+    [[ $((now - last_processed)) -ge "${VALID_SCHEDULES[$schedule]}" ]]
 }
 
 exec_rclone() {
     local fsname="$1"
 
-    echo "Running rclone:" "$RCLONE_STRATEGY" "${RCLONE_OPTIONS[@]}" "$fsname" "${RCLONE_REMOTE}:${fsname}"
-    rclone "$RCLONE_STRATEGY" "${RCLONE_OPTIONS[@]}" "/${fsname}" "${RCLONE_REMOTE}:${fsname}"
+    local lockfile="${LOCKFILE_DIRECTORY}/${fsname//\//-}-${RCLONE_REMOTE}.lck"
+    echo "Acquiring lock $lockfile"
+
+    (
+        flock --exclusive 200 --timeout "$LOCK_TIMEOUT"
+        echo "Lock $lockfile acquired"
+
+        echo "Running rclone:" "$RCLONE_STRATEGY" "${RCLONE_OPTIONS[@]}" "$fsname" "${RCLONE_REMOTE}:${fsname}"
+        rclone "$RCLONE_STRATEGY" "${RCLONE_OPTIONS[@]}" "/${fsname}" "${RCLONE_REMOTE}:${fsname}"
+    ) 200>"$lockfile"
 }
 
 #####################
@@ -52,14 +64,14 @@ exec_rclone() {
 # get all candidates for processing
 candidates=$(zfs get -H -o name,value -r "$SCHEDULE_PROPERTY_NAME" "$ZPOOL_NAME" -t "$ZFS_TYPE" | awk '$2 != "-"')
 
-while read -r fsname schedule ; do
+while read -r fsname schedule; do
     if [[ ! -v "VALID_SCHEDULES[${schedule}]" ]]; then
         echo "Invalid schedule $schedule set on $fsname; skipping" >&2
         continue
     fi
 
     last_processed=$(zfs get -H -o value "$PROCESSED_PROPERTY_NAME" "$fsname")
-    last_processed="${last_processed/-/0}"  # if not set, assume it's never been processed
+    last_processed="${last_processed/-/0}" # if not set, assume it's never been processed
 
     if should_process "$schedule" "$last_processed"; then
         exec_rclone "$fsname"
@@ -68,4 +80,4 @@ while read -r fsname schedule ; do
         echo "Skipping $fsname as it was last processed on $(date -d="$last_processed" +%c) and schedule is $schedule"
     fi
 
-done <<< "$candidates"
+done <<<"$candidates"
